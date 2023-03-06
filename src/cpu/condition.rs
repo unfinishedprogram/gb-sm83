356669b2f980c842e509b41e8f5628c9b785f477
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-06 14:05:32 -0500

    Add eq impl to enums

--- src/cpu/condition.rs
+++ src/cpu/condition.rs
@@ -1,24 +1,24 @@
 use std::fmt::{Debug, Formatter, Result};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Condition {
-	NZ,
-	Z,
-	NC,
-	C,
-	Always,
+    NZ,
+    Z,
+    NC,
+    C,
+    Always,
 }
 
 pub use Condition::*;
 
 impl Debug for Condition {
-	fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-		match self {
-			NZ => write!(f, "nz"),
-			Z => write!(f, "z"),
-			NC => write!(f, "nc"),
-			C => write!(f, "c"),
-			Always => write!(f, ""),
-		}
-	}
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            NZ => write!(f, "nz"),
+            Z => write!(f, "z"),
+            NC => write!(f, "nc"),
+            C => write!(f, "c"),
+            Always => write!(f, ""),
+        }
+    }
 }
